3b8e21e9c0d6bf9902070d8020e7d461470f5b46
Shohag Faraji <shohagfaraji2@gmail.com>
2025-11-26 16:21:20 +0600

    Replace 3D vector with 3D array for improved memory efficiency

---
+++ H2435. Paths in Matrix Whose Sum Is Divisible by K/RTB97_MMB99.cpp
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/paths-in-matrix-whose-sum-is-divisible-by-k/description/?envType=daily-question&envId=2025-11-26
+
+// Runtime Beats: 97.16%        Memory Beats: 99.05%
+
+class Solution {
+private:
+    static const int mod = 1000000007;
+
+public:
+    int numberOfPaths(vector<vector<int>>& grid, int K) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int dp[n][m][K];
+        memset(dp, 0, sizeof(dp));
+
+        const int g00 = grid[0][0];
+
+        for (int k = 0; k < K; k++)
+            if ((g00 + k) % K == 0)
+                dp[0][0][k] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                const int gij = grid[i][j];
+                for (int k = 0; k < K; k++) {
+                    const int nk = (k + gij) % K;
+                    if (i > 0)
+                        dp[i][j][k] = dp[i - 1][j][nk];
+                    if (j > 0)
+                        dp[i][j][k] += dp[i][j - 1][nk];
+                    dp[i][j][k] %= mod;
+                }
+            }
+        }
+        return dp[n - 1][m - 1][0];
+    }
+};
